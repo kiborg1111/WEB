3741fd87d76f7d518d34882f957e4947a3c3fd9e
Вячеслав <gorbunovslava2006@gmail.com>
2026-06-11 01:02:31 +1000

    fuck1

---
+++ header.php
@@ -0,0 +1,30 @@
+<header>
+    <div class = "header-container">
+        <div class = "logo">
+            <a href = "index.php">
+                <img src = "../kickzone/photo/logo.png" alt = "KickZone">
+            </a>
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href = "new.php">Новинки</a></li>
+                <li><a href = "catalog.php">Каталог</a></li>
+                <li><a href = "index.php">Мужское</a></li>
+                <li><a href = "index.php">Женское</a></li>
+                <li><a href = "index.php">О нас</a></li>
+            </ul>
+        </nav>
+         <div class="header-icons">
+            <a href="#" class="icon-link" aria-label="Поиск">
+                <i class="fas fa-search"></i>
+            </a>
+            <a href="#" class="icon-link" aria-label="Корзина">
+                <i class="fas fa-shopping-bag"></i>
+            </a>
+            <a href="#" class="icon-link" aria-label="Личный кабинет">
+                <i class="fas fa-user-circle"></i>
+            </a>
+        </div>
+    </div>
+</header>
---
+++ main.php
@@ -0,0 +1,50 @@
+<main>
+    <div class="main">
+        <div class="main-container">
+            <div class="title">
+                Кроссовки <br> которых <br> нет у всех
+            </div>
+        </div>
+        <div class="catalog-container">
+            <div class="main-catalog">
+                <div class="catalog-image">
+                    <img src="../kickzone/photo/title.jpg" alt="Каталог">
+                </div>
+
+                <div class="catalog-line"></div>
+
+                <div class="catalog-button">
+                    <a href="catalog.php" class="btn-catalog">Каталог</a>
+                </div>
+            </div>
+        </div>
+        <div class="scroll-container">
+            <div class="group">
+                
+                <!-- Карточка 1 - только рамка -->
+                <div class="product-card"></div>
+                
+                <!-- Карточка 2 - только рамка -->
+                <div class="product-card"></div>
+                
+                <!-- Карточка 3 - только рамка -->
+                <div class="product-card"></div>
+                
+                <!-- Карточка 4 - только рамка -->
+                <div class="product-card"></div>
+                
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+                <div class="product-card"></div>
+            </div>
+        </div>
+        
+    </div>
+</main>
