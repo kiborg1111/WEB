f2e6f378b1f81974ed37e6cd957ac190dab6d1e4
bgangval <chimitov.m@mail.ru>
2026-06-16 22:21:03 +1000

    Fix session start

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 <link rel="stylesheet" href="/kickzone/style/header.css">
 <div class="header">
